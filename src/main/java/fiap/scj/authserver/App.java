aadb9a05f8b44edd6a0a50572f1649a583d472b3
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-03 20:42:36 -0300

    Oauth

---
+++ src/main/java/fiap/scj/authserver/App.java
@@ -0,0 +1,13 @@
+package fiap.scj.authserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
