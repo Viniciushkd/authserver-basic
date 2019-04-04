e560331e28acb9d12a25d858e83f5781a6f89ab3
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-03 22:06:25 -0300

    Oauth

--- src/main/java/fiap/scj/seguranca/Application.java
+++ src/main/java/fiap/scj/seguranca/Application.java
@@ -5,6 +5,7 @@ import java.security.Principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,7 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 	
+	@RequestMapping("/validateUser")
 	public Principal user(Principal user) {
 		return user;
 	}
